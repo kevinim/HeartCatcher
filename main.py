7be9baa1b154223ff62f50949ff3a4e94e1d8672
kevinim <hwangbinim@gmail.com>
2021-11-06 15:38:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 # Creating the App
 
 from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.uix.bottomsheet import MDCustomBottomSheet
+from kivy.factory import Factory
+
+Window.size = (500,800)
+
 
 class Myapp(MDApp):
-	def build(self):
+	def build(self):       
 		return
-
+    
+#def open_bottom_sheet(self):
+#		self.obj = MDCustomBottomSheet(screen = Factory.CustomBottomSheet)
+    
 Myapp().run()
 
